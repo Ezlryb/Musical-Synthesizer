e4d21806422c95ab5792456fda11481de02d20cc
Ezlryb <22lbuchanan@wakatipu.school.nz>
2026-05-24 17:26:05 +1200

    Keyboard is wokring

--- GUI.py
+++ GUI.py
@@ -13,19 +13,34 @@ class Note:
         self.rect = normal_img.get_rect()
         self.location = location
         self.rect.topleft = location
-        
-    def hovered(self):
-        self.screen.blit(self.hovered_img, self.location)
-    
-    def pressed(self):
+        self.state = 'normal'
+        self.note = py.sndarray.make_sound(Wave(base_frequency,'squ',0.5,44100,1,1,1,1).wav.copy())
+
+    def play_note(self):
+        self.note = py.sndarray.make_sound(Wave(self.base_frequency,'squ',0.5,44100,1,1,1,1).wav.copy())
+        self.note.play()
+        self.state = 'pressed'
         self.screen.blit(self.pressed_img, self.location)
 
-    def released(self, pos):
-        if self.rect.collidepoint(pos):
+    def release_note(self, mouse_pos):
+        self.note.fadeout(20)
+        self.state = 'normal'
+        if self.rect.collidepoint(mouse_pos):
             self.screen.blit(self.hovered_img, self.location)
         else:
             self.screen.blit(self.normal_img, self.location)
 
+def mouse_note_check(notes):
+    mouse_pos = py.mouse.get_pos()
+    if py.mouse.get_pressed()[0]:
+        for note in notes:
+            if note.rect.collidepoint(mouse_pos) and note.state == 'normal':
+                note.play_note()
+            elif note.state == 'pressed' and not note.rect.collidepoint(mouse_pos):
+                note.release_note(mouse_pos)
+    else:
+        for note in notes:
+            note.release_note(mouse_pos)
 
 
 if __name__ == "__main__":
@@ -41,17 +56,14 @@ if __name__ == "__main__":
     running = True
     notes = []
     for i in range(1, 22):
-        key_normal = py.image.load('Resources/White_Key_Normal.png')
         key_pressed = py.image.load('Resources/White_Key_Pressed.png')
         key_hovered = py.image.load('Resources/White_Key_Hovered.png')
+        key_normal = py.image.load('Resources/White_Key_Normal.png')
         screen.blit(key_hovered, (KEYBOARD_X+i*24, KEYBOARD_Y))
         print(TWELVE_TONE_EQUAL_TEMP_FREQUENCIES[i+40])
         note = Note(key_normal, key_hovered, key_pressed, TWELVE_TONE_EQUAL_TEMP_FREQUENCIES[i+40], (KEYBOARD_X+i*24, KEYBOARD_Y), screen)
-        
         notes.append(note)
         note_clicked = False 
-
-    
     while running:
         for event in py.event.get():
             if event.type == py.QUIT:
@@ -59,29 +71,11 @@ if __name__ == "__main__":
             elif event.type == py.KEYDOWN:
                 if event.key == py.K_ESCAPE:
                     running = False
-                if event.key == py.K_1:
-                    sound_1 = py.sndarray.make_sound(Wave(600,'squ',0.5,44100,1,1,1,1).wav.copy())
-                    sound_1.play()
-                if event.key == py.K_2:
-                    sound_2 = py.sndarray.make_sound(Wave(650,'sin',0.5,44100,1,1,1,1).wav.copy())
-                    sound_2.play()
-            elif event.type == py.KEYUP:
-                if event.key == py.K_1:
-                    sound_1.stop()
-                if event.key == py.K_2:
-                    sound_2.stop()
             elif event.type == py.MOUSEBUTTONDOWN:
-                for note in notes:
-                    if note.rect.collidepoint(py.mouse.get_pos()):
-                        sound_3 = py.sndarray.make_sound(Wave(note.base_frequency,'saw',0.5,44100,1,1,1,1).wav.copy())
-                        sound_3.play()
-                        note.pressed()
-                        note_clicked = True
+                mouse_note_check(notes)
             elif event.type == py.MOUSEBUTTONUP:
-                if note_clicked:
-                    note.released(py.mouse.get_pos())
-                    note_clicked = False 
-                    sound_3.fadeout(50)
-                
+                mouse_note_check(notes)
+            elif event.type == py.MOUSEMOTION:
+                mouse_note_check(notes)       
         py.display.flip()
     quit()
